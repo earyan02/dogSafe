94e2c15cddad07ba7a65679b9c626c830008330c
earyan02 <earyan02@gmail.com>
2023-10-13 18:26:41 -0400

    connecting json

---
+++ script.js
@@ -0,0 +1,32 @@
+
+let foodHeader = document.getElementById("foodName");
+
+fetch('./foodList.json')
+    .then(res => res.json())
+    .then(foodList => {
+        console.log(foodList);
+        foodHeader.innerText = foodList;
+    });
+
+
+// const data = new Promise((resolve, reject) => {
+//     fetch('./foodList.json').then(response=> {resolve(response.json())}).catch(err=>reject(err))
+// })
+
+// let foodDisplay = document.getElementById(foodName)
+// let food = data.stringify(data[1])
+
+// const data = new Promise((resolve, reject) => {
+//     fetch('./foodList.json').then(response=>response.json()).then(data=>console.log(data[1]))
+//     updateFoodName(food);
+// })
+
+// function updateFoodName (food) {
+//     foodDisplay.innerText = food
+// }
+// let food = JSON.parse(data);
+
+
+// function updateCount(count) {
+//     counter.innerText = count;
+// }
